2f0d6cfca008bffd53e9d12eee2089e9cc1f0570
root <root@debian.local>
2018-04-28 15:15:58 +0100

    set(, ) now works with optionals

--- template.php
+++ template.php
@@ -48,7 +48,7 @@ class Html {
   }
 
   public function set($key, $val) {
-    $this->file = preg_replace("/#{ . $key  . }/", $val, $this->file);
+    $this->file = preg_replace("/(#|\?){ . $key  . }/", $val, $this->file);
   }
 
   public function match() {
